897058c0da2230297d8677ef2b06cbd92c96bada
rakeshnetsil <pb.rakesh90@gmail.com>
2021-08-11 17:39:33 +0530

    Register first endpoint

--- apis/version1/route_jobs.py
+++ apis/version1/route_jobs.py
@@ -2,15 +2,9 @@ from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import Session
 
 from db.session import get_db
-from db.models.jobs import Job
 from schemas.jobs import JobCreate,ShowJob
-from db.repository.jobs import (create_new_job,
-        retreive_job,list_jobs,
-        update_job_by_id,
-        delete_job_by_id)
-from apis.version1.route_login import get_current_user_from_token
-from db.models.users import User
-from typing import List
+from db.repository.jobs import create_new_job
+
 
 router = APIRouter()
 
