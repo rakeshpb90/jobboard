f86662ba0edf0cce1cfa9744631bf12698cc1185
rakeshnetsil <pb.rakesh90@gmail.com>
2021-08-11 11:20:50 +0530

    fourth

--- db/models/users.py
+++ db/models/users.py
@@ -4,14 +4,11 @@ from sqlalchemy.orm import relationship
 from db.base_class import Base
 
 
-class Job(Base):
+class User(Base):
     id = Column(Integer,primary_key=True,index=True)
-    title = Column(String,nullable=False)
-    company = Column(String,nullable=False)
-    company_url = Column(String)
-    location = Column(String,nullable=False)
-    description = Column(String)
-    date_posted = Column(Date)
-    is_active = Column(Boolean(),default = True)
-    owner_id = Column(Integer,ForeignKey('user.id'))
-    owner = relationship("User",back_populates="jobs")
+    username = Column(String,unique=True,nullable=False)
+    email = Column(String,nullable=False,unique=True,index=True)
+    hashed_password = Column(String,nullable=False)
+    is_active = Column(Boolean(),default=True)
+    is_superuser = Column(Boolean(),default=False)
+    jobs = relationship("Job",back_populates="owner")
