a7c76ba25665c92fa6c4c43a38369265eb494607
rakeshnetsil <pb.rakesh90@gmail.com>
2021-08-11 15:54:07 +0530

    Register first endpoint

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from fastapi import FastAPI
 from core.config import settings
 from db.session import engine
 from db.base import Base
-from apis.version1.route_users import router
+from apis.base import api_router
 
 
+def include_router(app):
+    app.include_router(api_router)
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    include_router(app) 
     create_tables()
-    print(router)
-    app.include_router(router)
     return app
 
 
