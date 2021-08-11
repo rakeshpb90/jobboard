55661aea1f6279a7aa8907d447e2f9ba650f4401
rakeshnetsil <pb.rakesh90@gmail.com>
2021-08-11 11:16:02 +0530

    fourth

--- db/models/users.py
+++ db/models/users.py
@@ -1,14 +1,17 @@
-from sqlalchemy import Column,Integer, String ,Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, Date , ForeignKey
 from sqlalchemy.orm import relationship
-
 
 from db.base_class import Base
 
-class User(Base):
+
+class Job(Base):
     id = Column(Integer,primary_key=True,index=True)
-    username = Column(String,unique=True,nullable=False)
-    email = Column(String,nullable=False,unique=True,index=True)
-    hashed_password = Column(String,nullable=False)
-    is_active = Column(Boolean(),default=True)
-    is_superuser = Column(Boolean(),default=False)
-    jobs = relationship("Job",back_populates="owner")
+    title = Column(String,nullable=False)
+    company = Column(String,nullable=False)
+    company_url = Column(String)
+    location = Column(String,nullable=False)
+    description = Column(String)
+    date_posted = Column(Date)
+    is_active = Column(Boolean(),default = True)
+    owner_id = Column(Integer,ForeignKey('user.id'))
+    owner = relationship("User",back_populates="jobs")
