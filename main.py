d86d607903b96e1e9d11a5a242e4c41f8831e297
rakeshnetsil <pb.rakesh90@gmail.com>
2021-08-11 09:33:29 +0530

    commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from core.config import settings
+from db.session import engine
+from db.base import Base
+
+
+
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+app = start_application()
+
+@app.get("/")
+def hello_api():
+    return {"name" : "rakesh"}
